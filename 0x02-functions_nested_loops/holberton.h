5a5d7496fbd7085017c6befd44470cc8a84d4d40
Lawal Yusuf Olayinka <lawalusersive@gmail.com>
2022-09-13 16:52:38 +0100

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,25 +1,24 @@
-int _putchar(char c);
+include "holberton.h"
 
-void print_alphabet(void);
+/**
+ * main - entry point
+ *
+ * Description: Prints Holberton with _putchar
+ *
+ * Return: always 0
+ */
 
-void print_alphabet_x10(void);
+int main(void)
+{
+	int i = 0;
+	char c;
+	char s[] = "Holberton\n";
 
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
+	while (i <= 9)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
