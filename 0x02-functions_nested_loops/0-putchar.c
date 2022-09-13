e65ef83f8ba44a50a7f765cc1518160be7e88ac5
Lawal Yusuf Olayinka <lawalusersive@gmail.com>
2022-09-13 20:52:14 +0100

    challenges

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,19 @@
-#include "holberton.h"
-
-
-/**
- * main - entry point
- *
- * Description: Prints Holberton with _putchar
- *
- * Return: always 0
- */
+#include "main.h"
+/** main - print putchar
+*Return: always 0
+*/
 
 int main(void)
 {
-	int i = 0;
-	char c;
-	char s[] = "Holberton\n";
+	_putchar("_")
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
-	while (i <= 9)
-	{
-		c = s[i];
-		_putchar(c);
-		i++;
-	}
 	return (0);
 }
