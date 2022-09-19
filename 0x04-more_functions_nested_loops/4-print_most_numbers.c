7517e53322797e60fe521c58cb672e702e6c6573
Lawal Yusuf Olayinka <lawalusersive@gmail.com>
2022-09-19 18:45:20 +0100

    challenges

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_most_numbers - prints 0 - 9 except 2, 3
- * Description: prints
- * Return: void
+ * print_most_numbers - prints the numbers from 0 to 9
+ * except 2 and 4
+ * Return: no return
  */
-
 void print_most_numbers(void)
 {
-	int a = 0;
+	int num;
 
-	while (a <= 9)
+	for  (num = '0'; num <= '9'; num++)
 	{
-		if (a != 2 && a != 4)
+		if (num != '2' && num != '4')
 		{
-			_putchar(a + '0');
+			_putchar(num);
 		}
-		a++;
 	}
 	_putchar('\n');
 }
+
