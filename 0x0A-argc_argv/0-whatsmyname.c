433ff3a5ccd4059b54d080b9794d109ad7423ff8
Lawal Yusuf Olayinka <lawalusersive@gmail.com>
2022-10-02 17:51:57 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+*main -print its name, followed by a new line
+ */
+ int main(int argc _attribute_((unused)), char *argv[])
+    {
+        printf("%s\n", argv[0]);
+        return (0);
+    }
