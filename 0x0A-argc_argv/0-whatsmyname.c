1a6a6edae98672ae73b3ef64e0993849479002e7
Lawal Yusuf Olayinka <lawalusersive@gmail.com>
2022-10-02 17:54:00 -0700

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 /**
 *main -print its name, followed by a new line
  */
- int main(int argc _attribute_((unused)), char *argv[])
+ int main(int argc __attribute__((unused)), char *argv[])
     {
         printf("%s\n", argv[0]);
         return (0);
